4e34a2b3e4e4f5253dc75df66026307325ce4a63
Gabriella Muniz <gabriellamunizzz08@gmail.com>
2024-04-08 19:20:17 -0300

    Continuanão da parte de estilização e lógica

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    //Chave da API
     const apiKey = 'live_uWECtJEa9IWjKvq7nGXj88oREmwmJHRWPXTkirBDlcLooZmYD2jqdmGbqJXheA6L';
-    //URL da API 
     const apiUrl = `https://api.thecatapi.com/v1/breeds?api_key=${apiKey}`;
-    //Chamando elemento HTML
     const card = document.getElementById('card');
 
-    //solicitação à API para obter informações
     fetch(apiUrl, {
         headers: {
             'x-api-key': apiKey
@@ -14,23 +10,34 @@ document.addEventListener("DOMContentLoaded", function() {
     })
     .then(response => response.json())
     .then(data => {
-        data.forEach(cat => {
-            const catCard = createCatCard(cat);
-            card.appendChild(catCard);
-        });
+        console.log(data)
+        window.catBreeds = data;
+        displayCatBreeds(window.catBreeds);
     })
     .catch(error => console.error('Erro ao buscar informações das raças de gatos:', error));
 
-    // Função para criar um card 
+    function displayCatBreeds(catBreeds) {
+        card.innerHTML = '';
+        catBreeds.forEach(cat => {
+            const catCard = createCatCard(cat);
+            card.appendChild(catCard);
+        });
+
+        createChart('temperament-chart', getTopBreeds(catBreeds, 'affection_level', 5), 'Nível de Afeto');
+        createChart('social-needs-chart', getTopBreeds(catBreeds, 'social_needs', 5), 'Necessidade Social');
+        createChart('life-expectancy-chart', getTopLifeExpectancyBreeds(catBreeds, 5), 'Expectativa de Vida Média');
+        createChart('affection-level-chart', getTopBreeds(catBreeds, 'affection_level', 5), 'Nível de Afeto');
+        createChart('energy-level-chart', getTopBreeds(catBreeds, 'energy_level', 5), 'Nível de Energia');
+    }
+
     function createCatCard(cat) {
         const catCard = document.createElement('div');
         catCard.classList.add('cat-card');
+        const imageUrl = cat.image ? cat.image.url : 'placeholder.jpg';
 
-
-        // Preenchendo o conteúdo do card com informações de forma dinâmica
         catCard.innerHTML = `
             <h2 class="cat-name">${cat.name}</h2>
-            <img class="cat-image" src="${cat.image.url}" alt="${cat.name}">
+            <img class="cat-image" src="${imageUrl}" alt="${cat.name}">
             <p><strong>Origem:</strong> <span class="cat-origin">${cat.origin}</span></p>
             <p><strong>Descrição:</strong> <span class="cat-description">${cat.description}</span></p>
             <p><strong>Temperamento:</strong> <span class="cat-temperament">${cat.temperament}</span></p>
@@ -40,6 +47,53 @@ document.addEventListener("DOMContentLoaded", function() {
 
         return catCard;
     }
+
+    function createChart(chartId, breeds, label) {
+        const ctx = document.getElementById(chartId).getContext('2d');
+        const chart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: breeds.map(breed => breed.name),
+                datasets: [{
+                    label: label,
+                    data: breeds.map(breed => breed.value),
+                    backgroundColor: 'rgba(75, 192, 192, 0.5)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        max: Math.max(...breeds.map(breed => breed.value)) + 1
+                    }
+                }
+            }
+        });
+    }
+
+    function getTopBreeds(catBreeds, attribute, count) {
+        const sortedBreeds = catBreeds.sort((a, b) => b[attribute] - a[attribute]);
+        return sortedBreeds.slice(0, count).map(breed => ({ name: breed.name, value: breed[attribute] }));
+    }
+
+    function getTopLifeExpectancyBreeds(catBreeds, count) {
+        const breedsWithLifeExpectancy = catBreeds.filter(cat => cat.life_span);
+        const sortedBreeds = breedsWithLifeExpectancy.sort((a, b) => {
+            const lifeSpanA = parseInt(a.life_span.split(' ')[0]);
+            const lifeSpanB = parseInt(b.life_span.split(' ')[0]);
+            return lifeSpanB - lifeSpanA;
+        });
+        return sortedBreeds.slice(0, count).map(breed => ({ name: breed.name, value: parseInt(breed.life_span.split(' ')[0]) }));
+    }
+
+    function buscar() {
+        const searchTerm = document.getElementById('search').value.toLowerCase();
+        const raceFilter = window.catBreeds.filter(cat => cat.name.toLowerCase().includes(searchTerm));
+        displayCatBreeds(raceFilter);
+    }
+
+    const buttonSearch = document.querySelector('.button-search');
+    buttonSearch.addEventListener('click', buscar);
 });
-
-
